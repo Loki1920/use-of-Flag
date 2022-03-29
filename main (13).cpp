c81f5f30733eff70544a0ba74fa2e1a1e904f6a6
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2022-03-29 12:33:55 +0530

    Add files via upload

---
+++ main (13).cpp
@@ -0,0 +1,23 @@
+// use of flag 
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+  int n;
+  cout<<"enter number:";
+  cin>>n;
+  bool flag = 0;
+
+  for(int i=2;i<=sqrt(n);i++){
+    if(n%i==0){
+      cout<<"non-prime";
+      flag = 1;
+      break;
+    }
+  }
+  if(flag==0){
+    cout<<"prime";
+  }
+  return 0;
+}
